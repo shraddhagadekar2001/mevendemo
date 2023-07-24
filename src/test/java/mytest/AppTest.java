b7dcd6017bfa33065cdee2ad41e79e032acdc5fc
kidda24 <118981876+kidda24@users.noreply.github.com>
2023-07-24 00:49:37 -0700

    Update AppTest.java

--- src/test/java/mytest/AppTest.java
+++ src/test/java/mytest/AppTest.java
@@ -22,7 +22,7 @@ public class AppTest  {
 
 
 	@Test
-	public void testApp1() {
+	public void testApp2() {
 		Test1 t = new Test1();
 		String expected = "AAAA";
 		String actual = t.palindrome(expected);
